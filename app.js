8edf03b641733a25117478a439def1a98fa24931
Karim Ouazzou <karimouazzou@gmail.com>
2024-02-06 11:56:10 +0100

    setup routes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const auth = require('./routes/auth/auth')
 
 app.listen(8089, () => {
     console.log("listening on port 8089")
 })
+
+app.use('/auth',auth)
