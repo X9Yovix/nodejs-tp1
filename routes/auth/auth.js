db38d1cce5063af68302447a1e3f5a4268fd5699
Karim Ouazzou <karimouazzou@gmail.com>
2024-02-06 12:27:50 +0100

    q2

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router();
+const path = require('path');
 
 router.get('/login', (req,res) => {
     res.send("<p>message en html</p>")
+})
+
+router.get('/register', (req,res) => {
+    const filePath = path.join(__dirname, '../../view/register.html');
+    res.sendFile(filePath);
 })
 
 module.exports = router
